f80ec8e1d991ba9c58ffdc8d05e2cd4aa121d8d3
Jonathan Wilde <hello@jwilde.me>
2015-04-01 21:01:19 -0400

    Add InputPopupWrapper with very cursory test.

---
+++ src/InputPopupWrapper.js
@@ -0,0 +1,55 @@
+var React = require('react/addons');
+
+var {cloneWithProps} = React.addons;
+
+var joinClasses = require('react/lib/joinClasses');
+var getUniqueId = require('./helpers/getUniqueId');
+
+/**
+ * <InputPopupWrapper> enables us to attach a popup menu like <ListPopup>
+ * to an <input> element in an WAI ARIA compliant manner.
+ */
+var InputPopupWrapper = React.createClass({
+
+  propTypes: {
+    isOpen: React.PropTypes.bool,
+    popup: React.PropTypes.element
+  },
+
+  getDefaultProps: function() {
+    return {
+      isOpen: false,
+      popup: <div/>
+    };
+  },
+
+  getInitialState: function() {
+    return {
+      id: getUniqueId('InputPopupWrapper')
+    };
+  },
+
+  render: function() {
+    return (
+      <div className="InputPopupWrapper">
+        {cloneWithProps(React.Children.only(this.props.children), {
+          'aria-haspopup': 'true',
+          'aria-owns': this.state.id,
+          'aria-expanded': this.props.isOpen+'',
+          'className': 'InputPopupWrapper-input'
+        })}
+        <div
+          id={this.state.id}
+          className={joinClasses(
+            'InputPopupWrapper-popup',
+            this.props.isOpen && 'InputPopupWrapper-popup--isOpen'
+          )}>
+          {this.props.popup}
+        </div>
+      </div>
+    );
+  }
+
+});
+
+module.exports = InputPopupWrapper;
---
+++ src/__tests__/InputPopupWrapper-test.js
@@ -0,0 +1,28 @@
+var InputPopupWrapper = require('../InputPopupWrapper');
+var React = require('react/addons');
+
+var {TestUtils} = React.addons;
+var {Simulate} = React.addons.TestUtils;
+
+var expect = require('expect');
+var emptyFunction = require('../helpers/emptyFunction');
+
+describe('InputPopupWrapper', function() {
+
+  it('should decorate <input> and the popup with ARIA attrs', function() {
+    var ctx = TestUtils.renderIntoDocument(
+      <InputPopupWrapper isOpen={true}><input/></InputPopupWrapper>
+    );
+
+    var input = TestUtils.findRenderedDOMComponentWithTag(ctx, 'input');
+    var menu = TestUtils.findRenderedDOMComponentWithClass(ctx, 'InputPopupWrapper-popup');
+
+    var inputAttributes = input.getDOMNode().attributes;
+    var menuAttributes = menu.getDOMNode().attributes;
+
+    expect(inputAttributes['aria-expanded'].value).toBe('true');
+    expect(inputAttributes['aria-haspopup'].value).toBe('true');
+    expect(inputAttributes['aria-owns'].value).toBe(menuAttributes['id'].value);
+  });
+
+});
