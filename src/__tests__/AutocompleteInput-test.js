045d4f91622cd69e0867e869f9274d504591f4d6
Jonathan Wilde <hello@jwilde.me>
2015-04-01 21:00:56 -0400

    Dropping tab completion support.

--- src/__tests__/AutocompleteInput-test.js
+++ src/__tests__/AutocompleteInput-test.js
@@ -8,7 +8,6 @@ var expect = require('expect');
 var emptyFunction = require('../helpers/emptyFunction');
 
 const KEY_RETURN = 13;
-const KEY_TAB = 9;
 const KEY_BACKSPACE = 8;
 
 var AutocompleteInputTestWrapper = React.createClass({
@@ -55,29 +54,33 @@ var AutocompleteInputTestWrapper = React.createClass({
 describe('AutocompleteInput', function() {
 
   it('shows some text, and supports typing', function() {
-    var wrapper = TestUtils.renderIntoDocument(
+    var ctx = TestUtils.renderIntoDocument(
       <AutocompleteInputTestWrapper initialValue="hello"/>
     );
 
     // Ensure that the text that we provided actually shows up in the input
-    var input = TestUtils.findRenderedDOMComponentWithTag(wrapper, 'input');
-    expect(input.getDOMNode().value).toBe('hello');
+    var input = TestUtils.findRenderedDOMComponentWithTag(ctx, 'input');
+    var inputNode = input.getDOMNode();
+
+    expect(inputNode.value).toBe('hello');
 
     // Ensure that changes that we supply actually show up in the input
     Simulate.change(input, {target: {value: 'hello!'}});
     Simulate.keyDown(input, {key: '!'});
     Simulate.keyUp(input, {key: '!'});
     
-    expect(input.getDOMNode().value).toBe('hello!');
+    expect(inputNode.value).toBe('hello!');
   });
 
   it('shows, selects, and enables completion of typeahead text', function() {
-    var wrapper = TestUtils.renderIntoDocument(
+    var ctx = TestUtils.renderIntoDocument(
       <AutocompleteInputTestWrapper completionValue="California"/>
     );
 
     // Ensure that there's no typehead
-    var input = TestUtils.findRenderedDOMComponentWithTag(wrapper, 'input');
+    var input = TestUtils.findRenderedDOMComponentWithTag(ctx, 'input');
+    var inputNode = input.getDOMNode();
+
     expect(input.getDOMNode().value).toBe('');
 
     // Ensure that the typeahead shows up case-insensitively and is selected
@@ -85,20 +88,20 @@ describe('AutocompleteInput', function() {
     Simulate.keyDown(input, {key: 'c'});
     Simulate.keyUp(input, {key: 'c'});
     
-    expect(input.getDOMNode().value).toBe('california');
-    expect(input.getDOMNode().selectionStart).toBe(1);
-    expect(input.getDOMNode().selectionEnd).toBe(10);
+    expect(inputNode.value).toBe('california');
+    expect(inputNode.selectionStart).toBe(1);
+    expect(inputNode.selectionEnd).toBe(10);
 
     // Ensure that we can tab complete the value
-    Simulate.keyDown(input, {keyCode: KEY_TAB});
+    Simulate.keyDown(input, {keyCode: KEY_RETURN});
 
-    expect(input.getDOMNode().value).toBe('California');
-    expect(input.getDOMNode().selectionStart).toBe(10);
-    expect(input.getDOMNode().selectionEnd).toBe(10);
+    expect(inputNode.value).toBe('California');
+    expect(inputNode.selectionStart).toBe(10);
+    expect(inputNode.selectionEnd).toBe(10);
   });
 
   it('omits the typehead if the completion text does not match', function() {
-    var wrapper = TestUtils.renderIntoDocument(
+    var ctx = TestUtils.renderIntoDocument(
       <AutocompleteInputTestWrapper 
         completionValue="California"
         initialValue="h"
@@ -106,17 +109,19 @@ describe('AutocompleteInput', function() {
     );
 
     // Ensure that there's no typehead
-    var input = TestUtils.findRenderedDOMComponentWithTag(wrapper, 'input');
-    expect(input.getDOMNode().value).toBe('h');
+    var input = TestUtils.findRenderedDOMComponentWithTag(ctx, 'input');
+    var inputNode = input.getDOMNode();
+
+    expect(inputNode.value).toBe('h');
 
     // Ensure that there's still no typeahead
     Simulate.change(input, {target: {value: 'hi'}});
     Simulate.keyDown(input, {key: 'i'});
     Simulate.keyUp(input, {key: 'i'});
 
-    expect(input.getDOMNode().value).toBe('hi');
-    expect(input.getDOMNode().selectionStart).toBe(2);
-    expect(input.getDOMNode().selectionEnd).toBe(2);
+    expect(inputNode.value).toBe('hi');
+    expect(inputNode.selectionStart).toBe(2);
+    expect(inputNode.selectionEnd).toBe(2);
   });
 
 });
